1e0623d05885da602e30d78240d57c2f77ada46f
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-07-24 13:50:23 +0700

    update webtitle

--- lab03-6/src/router/index.ts
+++ lab03-6/src/router/index.ts
@@ -3,6 +3,10 @@ import HomeView from '@/views/HomeView.vue'
 import PassengerDetailsView from '@/views/PassengerDetailsView.vue'
 import AirlineDetailsView from '@/views/AirlineDetailsView.vue'
 
+type CustomRouteMeta = {
+  title?: string;
+};
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,6 +14,9 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Home - Flight Booking',
+      },
     },
     {
       path: '/about',
@@ -18,28 +25,50 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/AboutView.vue'),
+      meta: {
+        title: 'About - Flight Booking',
+      },
     },
     {
       path: '/passenger/:id',
       name: 'passenger-details',
       component: PassengerDetailsView,
+      meta: {
+        title: 'Passenger Details - Flight Booking',
+      },
     },
     {
       path: '/airline/:airlineId',
       name: 'airline-details',
       component: AirlineDetailsView,
+      meta: {
+        title: 'Airline Details - Flight Booking',
+      },
     },
     {
       path: '/resource-not-found',
       name: 'resource-not-found',
       component: () => import('@/views/ResourceNotFoundView.vue'),
+      meta: {
+        title: 'Resource Not Found - Flight Booking',
+      },
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: () => import('@/views/NotFoundView.vue'),
+      meta: {
+        title: 'Not Found - Flight Booking',
+      },
     },
   ],
 })
+
+router.afterEach((to) => {
+  const defaultTitle = 'Flight Booking';
+  const meta = to.meta as CustomRouteMeta;
+  document.title = meta.title || defaultTitle;
+});
+
 
 export default router
